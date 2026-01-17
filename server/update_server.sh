eea05bc65d61911b51675a08e997b6888b5a4eec
ftyszyx <whyzi@qq.com>
2026-01-17 23:43:52 +0800

    update docker

--- server/update_server.sh
+++ server/update_server.sh
@@ -1,6 +1,7 @@
-# stop server
-docker compose -f docker-compose.release.yml down server
+
 # pull new server image
 docker compose -f docker-compose.release.yml pull server 
+# stop server
+docker compose -f docker-compose.release.yml down server
 # start server
 docker compose -f docker-compose.release.yml up -d --force-recreate server
